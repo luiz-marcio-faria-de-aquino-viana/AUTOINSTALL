72b59793d6e2af07af787408054fcc03bd5865a0
Luiz Marcio Faria de Aquino Viana, Pos-D.Sc. <151483374+luiz-marcio-faria-de-aquino-viana@users.noreply.github.com>
2026-09-15 21:54:29 -0300

    AI_AUTOINSTALL – COMPLETO COM FIACAO
    Copyright (C) 1989-2026 TLMV CONSULTORIA E SISTEMAS EIRELI
    AI_AUTOINSTALL – COMPLETO COM FIACAO, LUMENS, PRP2BMP, PRP2LST e QUEUE – EM TODAS AS VERSOES
    Autor: Luiz Marcio Faria de Aquino Viana, Pos-D.Sc. - Engenheiro
    Registro: 20001035871 CREA-RJ 
    Graduacao: Engenharia Eletrica
    Unidade: UERJ - Universidade do Estado do Rio de Janeiro
    Curso: Engenharia Eletrica com Enfase em Engenharia de Sistemas e Computação
    Mestrado/Doutorado/Pos-Doutoramento: Programa de Engenharia de Sistemas e Computacao - PESC
    Unidade: COPPE/UFRJ - Universidade Federal do Rio de Janeiro
    Area: Arquitetura de Computadores e Sistemas Operacionais
    Unico Socio e Administrador da Empresa - Desde: 02/08/2000

---
+++ 001-SRC/AI_AUTOINSTALL/AI2_30/MKVAR.C
@@ -0,0 +1,81 @@
+
+#include<stdio.h>
+#include<string.h>
+
+#define RSLT_OK  0
+#define RSLT_ERR 1
+
+typedef char str_t [256];
+
+char *Piece(char *s, int in, int fn);
+
+main(int argc, char *argv[])
+{
+  FILE *src_ptr;
+  FILE *dst_ptr;
+
+  str_t src_file, dst_file;
+  str_t s1, s2;
+
+  int rst = RSLT_ERR;
+
+  printf("                þþþ MkVar v1.0 þþþ\n");
+  printf("Copyright (C) 1995 by Luiz Marcio F A Viana, 3/15/95.\n\n");
+
+  if(argc)
+  {
+    strcat(strcpy(src_file, argv[1]), ".PRN");
+    strcat(strcpy(dst_file, argv[1]), ".SCR");
+    if((src_ptr = fopen(src_file, "r")) != NULL)
+    {
+      if((dst_ptr = fopen(dst_file, "w")) != NULL)
+      {
+	if((fgets(s1, sizeof(str_t), src_ptr) != NULL) && (fgets(s1, sizeof(str_t), src_ptr) != NULL))
+	{
+	  printf("Processing... ");
+	  while(fgets(s1, sizeof(str_t), src_ptr) != NULL)
+	  {
+	    strcpy(s2, s1);
+	    fprintf(dst_ptr, "SETVAR %s", Piece(s1, 0, 15));
+	    fprintf(dst_ptr, " %s", Piece(s2, 16, 31));
+	    printf(".");
+	  }
+	  fcloseall();
+	  rst = RSLT_OK;
+	}
+	else
+	{
+	  printf("ERR: Invalid file format.\n");
+	}
+      }
+      else
+      {
+	printf("ERR: Can't open destination file.\n");
+      }
+    }
+    else
+    {
+      printf("ERR: Can't open source file.\n");
+    }
+  }
+  else
+  {
+    printf("ERR: Invalid number of parameters.\n");
+  }
+  return(rst);
+}
+
+char *Piece(char *s, int in, int fn)
+{
+  int i;
+
+  for(i = in; i <= fn; i++)
+  {
+    if((s[i] == ' ') || (s[i] == '\0'))
+    {
+      s[i] = '\0';
+      break;
+    }
+  }
+  return(&s[in]);
+}
